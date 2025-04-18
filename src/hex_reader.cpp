01313c22e6dd4813c5a88a59720fa60e1db05bc2
tailsmonster <aroca.twelve@proton.me>
2025-04-18 18:28:55 -0400

    fixed weird formatting

--- src/hex_reader.cpp
+++ src/hex_reader.cpp
@@ -12,13 +12,13 @@ using std::string;
 
 vector<uint8_t> readRawBin(const string& path) {
   std::ifstream file(path, std::ios::binary);
-  return vector<uint8_t>((std::istreambuf_iterator<char>(file)),
-                          std::istreambuf_iterator<char>());
+  return vector<uint8_t>((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 };
 
 void printHex(const vector<uint8_t>& data) {
   for (size_t i = 0; i < data.size(); ++i) {
-    if (i % 16 == 0) std::cout << std::endl;
+    if (i % 16 == 0) 
+      std::cout << std::endl;
     std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(data[i]) << " ";
   }
   std::cout << std::dec << std::endl;
